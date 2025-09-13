b064ec9ca14dec625fe332e765a9484289767044
Kieu, QuocChien | ECMPD <ts-quocchien.kieu@rakuten.com>
2025-09-13 16:15:48 +0900

    Add commit condition

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,7 @@
-import { createStore, applyMiddleware } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
-import thunk from "redux-thunk";
 import rootReducer from "./rootReducer";
 
-const makeStore = () => createStore(rootReducer);
+const makeStore = () => configureStore({ reducer: rootReducer });
 
 export const wrapper = createWrapper(makeStore);
