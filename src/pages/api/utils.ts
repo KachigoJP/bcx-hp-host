57e2a82221da515fce2458e63fac1afbec141ff6
Kieu Chien <chienkieu1890@gmail.com>
2026-06-12 18:57:30 +0900

    Add DEBUG for google client

--- src/pages/api/utils.ts
+++ src/pages/api/utils.ts
@@ -1,6 +1,7 @@
 import { google } from "googleapis";
 
 export function getAuth() {
+  console.debug("Creating Google API client with provided credentials: ", process.env.GOOGLE_CLIENT_ID);
   const client = new google.auth.OAuth2(
     process.env.GOOGLE_CLIENT_ID,
     process.env.GOOGLE_CLIENT_SECRET,
